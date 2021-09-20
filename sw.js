7a5b475a9502054f37ecbd2dc7bde4c47adf522f
Dylan <heyraud.dylan@gmail.com>
2021-09-20 11:11:42 +0200

    sw

--- sw.js
+++ sw.js
@@ -116,10 +116,12 @@ self.addEventListener('fetch', (evt) => {
  
 });
 
+/*
+// 8. Notifications push : on commente les anciennes notifications ci dessous
+ 
 // 7.3 Notifications persistantes (envoyées depuis le service worker)
 self.registration.showNotification("Notification du SW", {
     body:"je suis une notification dite persistante",
-  
     // 7.4 Options de notifications grâce aux actions
     actions:[
         {action:"accept", title:"accepter"},
@@ -127,7 +129,12 @@ self.registration.showNotification("Notification du SW", {
     ]
 })
  
-/// 7.4 Options de notifications grâce aux actions
+// 7.3 Ecoute de l'événement close
+self.addEventListener("notificationclose", evt => {
+    console.log("Notification fermée", evt);
+})
+ 
+// 7.4 Options de notifications grâce aux actions
 // Ecouteur au clic d'un des deux boutons de la notification
 self.addEventListener("notificationclick", evt => {
     console.log("notificationclick evt", evt);
@@ -138,7 +145,20 @@ self.addEventListener("notificationclick", evt => {
     } else{
         console.log("vous avez cliqué sur la notification (pas sur un bouton)");
     }
-  
-    // 7.5 Fermer programmatiquement une notification
-    evt.notification.close();
+})
+*/
+
+	
+// 8.1 Intercepter une notification push
+self.addEventListener("push", evt => {
+    console.log("push event", evt);
+    console.log("data envoyée par la push notification :", evt.data.text());
+ 
+    // 8.1 afficher son contenu dans une notification
+    const title = evt.data.text();
+    const objNotification = {
+        body: "ça fonctionne", 
+        icon : "images/icons/icon-72x72.png"
+    };
+    self.registration.showNotification(title, objNotification);
 })
